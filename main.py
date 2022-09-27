66d1972fe5bcd1665611624316f53b707fcc980c
Kelvin de Reus <k_dereus@outlook.com>
2022-09-27 15:28:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import tkinter as tk
 from tkinter import ttk
-from ttkthemes import ThemedTk
 import random
-from tkinter import *
-import tkinter
-
 
 
 # Defining the actual game.
@@ -15,57 +12,66 @@ def game_code():
 def clicked_rock():
     game_code()
     if result_random == "rock":
-        output_game.configure(text="Shit, it's a tie!")
+        label_game_output.configure(text="Shit, it's a tie!")
     elif result_random == "paper":
-        output_game.configure(text="Shit, you lose!")
+        label_game_output.configure(text="Shit, you lose!")
     else:
-        output_game.configure(text="You won! Yihaaa!")
+        label_game_output.configure(text="You won! Yihaaa!")
 
 def clicked_paper():
     game_code()
     if result_random == "paper":
-        output_game.configure(text="Shit, it's a tie!")
+        label_game_output.configure(text="Shit, it's a tie!")
     elif result_random == "scissors":
-        output_game.configure(text="Shit, you lose!")
+        label_game_output.configure(text="Shit, you lose!")
     else:
-        output_game.configure(text="You won! Yihaaa!")
+        label_game_output.configure(text="You won! Yihaaa!")
 
 def clicked_chizzors():
     game_code()
     if result_random == "scissors":
-        output_game.configure(text="Shit, it's a tie!")
+        label_game_output.configure(text="Shit, it's a tie!")
     elif result_random == "rock":
-        output_game.configure(text="Shit, you lose!")
+        label_game_output.configure(text="Shit, you lose!")
     else:
-        output_game.configure(text="You won! Yihaaa!")
+        label_game_output.configure(text="You won! Yihaaa!")
 
 
 
+# Setting up defaults, main core stuff. Also setting the icon.
+root = tk.Tk()
+root.title("KDR")
+root.iconbitmap("favicon.ico")
+
+root.option_add("*tearOff", False) # This is always a good idea
+root.tk.call('source', 'forest-dark.tcl')
+ttk.Style().theme_use('forest-dark')
 
 
+# Top app text.
+label_welcome_top = ttk.Label(root, text="Rock, paper and scissors game!")
+label_welcome_top.pack(pady=15, padx=30)
 
-# GUI code below me.
-window = ThemedTk(theme='yaru')
+# User input buttonanananananas.
+btn_rock_input = ttk.Button(root, text="Rock", command=clicked_rock)
+btn_rock_input.pack(pady=5, padx=30)
 
-# Setting up core value's.
-window.title("Rock, paper, scissors!")
-window.geometry("300x185")
-window.iconbitmap(r"kdr.png")
+btn_paper_input = ttk.Button(root, text="Paper", command=clicked_paper)
+btn_paper_input.pack(pady=5, padx=30)
 
-# The different elements within the application (TXT, buttons etc).
-greeting_txt = Label(window, text="Rock, paper, scissors game!!!", width=40)
-greeting_txt.grid(column=6, row=0)
+btn_scissors_input = ttk.Button(root, text="Scissors", command=clicked_chizzors)
+btn_scissors_input.pack(pady=5, padx=30)
 
-btn = ttk.Button(window, text="Rock", command=clicked_rock, width=10)
-btn.grid(column=6, row=1, pady=5)
+# Output game area
+label_game_output = ttk.Label(root, text="")
+label_game_output.pack(pady=15, padx=30)
 
-btn_two = ttk.Button(window, text="Paper", command=clicked_paper, width=10)
-btn_two.grid(column=6, row=2, pady=5)
 
-btn_three = ttk.Button(window, text="Scissors", command=clicked_chizzors, width=10)
-btn_three.grid(column=6, row=3, pady=5)
+# Center the window, and set minsize
+root.update()
+root.minsize(root.winfo_width(), root.winfo_height())
+x_cordinate = int((root.winfo_screenwidth()/2) - (root.winfo_width()/2))
+y_cordinate = int((root.winfo_screenheight()/2) - (root.winfo_height()/2))
+root.geometry("+{}+{}".format(x_cordinate, y_cordinate))
 
-output_game = Label(window, text="")
-output_game.grid(column=6, row=4, sticky=tkinter.S, pady=5)
-
-window.mainloop()
+root.mainloop()
